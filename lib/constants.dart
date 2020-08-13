c70d6f04f7545db88271171eb545d404cf08797e
Mohamed El Balooty <mohamedelbalooty123@gmail.com>
2020-08-13 20:04:26 +0200

    remove un used color

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,5 +8,3 @@ const Color KBlackColor = Colors.black;
 const Color KBottomIcon = Color(0xFFffa83e);
 const Color KCustomContainerColor = Color(0xFFf9f7d8);
 const Color KFontColor = Color(0xFFa9afaf);
-//const Color KCustomFontColor = Color(0xFFced2d4);
-
